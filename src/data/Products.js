c71715ca5148fedbe30d6a44800554ffad2fcc5a
aisha-png <67974517+aisha-png@users.noreply.github.com>
2024-04-18 14:41:04 +0100

    fixed data layout in file

--- src/data/Products.js
+++ src/data/Products.js
@@ -5,16 +5,75 @@ import flour from "../../src/assets/apple_img.jpeg"
 import ladyFingers from "../../src/assets/apple_img.jpeg"
 
 const products = [
-    {id: 1, name: 'Apple', price: 0.49, image: apple, category: 'Fruits'},
-    {id: 2, name: 'Bread', price: 1.49, image: bread, category: 'Bakery'},
-    {id: 3, name: 'Semi-Skimmed Milk', price: 0.79, image: milk, category: 'Dairy'},
-    {id: 4, name: 'Plain Flour', price: 1.79, image: flour, category: 'Pantry Staples'},
-    {id: 5, name: 'Lady Fingers', price: 2.00, image: ladyFingers, category: 'Pantry Staples'},
-    {id: 6, name: 'Orange', price: 0.49, image: apple, category: 'Fruits'},
-    {id: 7, name: 'Baguette', price: 1.49, image: bread, category: 'Bakery'},
-    {id: 8, name: 'Whole Milk', price: 0.79, image: milk, category: 'Fruits'},
-    {id: 9, name: 'Self Raising Flour', price: 1.79, image: flour, category: 'Pantry Staples'},
-    {id: 10, name: 'Vanilla Extract', price: 2.00, image: ladyFingers,category: 'Pantry Staples'}
+    {
+        id: 1, 
+        name: 'Apple', 
+        price: 0.49, 
+        image: apple, 
+        category: 'Fruits'
+    },
+    {
+        id: 2, 
+        name: 'Bread', 
+        price: 1.49, 
+        image: bread, 
+        category: 'Bakery'
+    },
+    {
+        id: 3, 
+        name: 'Semi-Skimmed Milk', 
+        price: 0.79, 
+        image: milk, 
+        category: 'Dairy'
+    },
+    {
+        id: 4, 
+        name: 'Plain Flour', 
+        price: 1.79, 
+        image: flour, 
+        category: 'Pantry Staples'
+    },
+    {
+        id: 5, 
+        name: 'Lady Fingers', 
+        price: 2.00, 
+        image: ladyFingers, 
+        category: 'Pantry Staples'
+    },
+    {
+        id: 6, 
+        name: 'Orange', 
+        price: 0.49, 
+        image: apple, 
+        category: 'Fruits'
+    },
+    {
+        id: 7, 
+        name: 'Baguette', 
+        price: 1.49, 
+        image: bread, 
+        category: 'Bakery'
+    },
+    {
+        id: 8, 
+        name: 'Whole Milk', 
+        price: 0.79, 
+        image: milk, 
+        category: 'Fruits'
+    },
+    {
+        id: 9, 
+        name: 'Self Raising Flour', 
+        price: 1.79, 
+        image: flour, 
+        category: 'Pantry Staples'},
+    {
+        id: 10, 
+        name: 'Vanilla Extract', 
+        price: 2.00, 
+        image: ladyFingers,
+        category: 'Pantry Staples'
+    }
 ]
 
 export default products;
