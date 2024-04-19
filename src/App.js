0caf4c3afee09f36ad3b6436ffcb4177eb04f661
aisha-png <67974517+aisha-png@users.noreply.github.com>
2024-04-19 17:21:43 +0100

    added navbar component and rendered it

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from './pages/Home';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
 
   return (
     <div className="App">
+      <Navigation />
       <h4 style={{color: '#F06C00'}}>Sainsbury's</h4>
       <Home />
     </div>
